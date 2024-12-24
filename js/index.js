250f9eb6b47afc503d0f7d391d756ab9962fc823
Soham Ray <sohamray@Sohams-Laptop.local>
2024-12-24 10:25:27 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.getElementById('mobile-menu-button').addEventListener('click', () => {
+    document.getElementById('slideout-menu').classList.toggle('open');
+});
+
+document
+    .getElementById('mobile-close-menu-button')
+    .addEventListener('click', () => {
+        document.getElementById('slideout-menu').classList.toggle('open');
+    });
+
+
+
